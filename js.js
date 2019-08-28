b3812478af961dfe95df90c4adb869d71c0fe70d
Alex Sierra <alexandercsierra@gmail.com>
2019-08-28 16:43:38 -0400

    HTML also limited to seven guests, added some text clarification

--- js.js
+++ js.js
@@ -11,8 +11,20 @@ addGuest.addEventListener("submit", function(e){
     guestList.appendChild(newPara);
 
     //add content
-    newPara.textContent = value;
-    guestArray.push(value);
-    finalCount = guestArray.slice(0, 7);
-    console.log(finalCount);
+    //guestArray.push(value);
+    //var finalCount = guestArray.slice(0, 7);
+    //newPara.textContent = "The guest list includes: " + finalCount;
+    
+    if (guestArray[6] != null){
+        guestArray.slice(0, 7);
+        console.log(guestArray);
+    } else if (guestArray[5] != null){
+        guestArray.push(value);
+        newPara.textContent = "The final guest list includes: " + guestArray;
+    } else {
+        guestArray.push(value);
+        newPara.textContent = "The current guest list includes: " + guestArray;
+    }
+
 });
+
