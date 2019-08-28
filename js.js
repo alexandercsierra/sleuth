8481ea941cbe8ce1c170866a75fa9d9aedde9378
Alex Sierra <alexandercsierra@gmail.com>
2019-08-28 15:06:58 -0400

    Submit button now adds new paragraphs

--- js.js
+++ js.js
@@ -2,3 +2,18 @@ function writeText() {
     var words = document.getElementById("text").value;
     document.getElementById("guestList").innerHTML += (words + " ");
 }
+
+//add guest
+const addGuest = document.forms['onlyForm'];
+addGuest.addEventListener("submit", function(e){
+    e.preventDefault();
+    const value = addGuest.querySelector('input[type="text"]').value;
+
+    //create elements
+    const newPara = document.createElement("p");
+    const guestList = document.getElementById("guestList");
+    guestList.appendChild(newPara);
+
+    //add content
+    newPara.textContent = value;
+});
