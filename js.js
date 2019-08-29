f736383b55e8c86880ff13098da020e9cc4a45bc
Alex <alexandercsierra@gmail.com>
2019-08-29 01:53:25 -0400

    randomly selects murder victim and displays in text prompt

--- js.js
+++ js.js
@@ -1,5 +1,11 @@
 var guestArray=[];
 
+function whoIsDead() {
+    return guestArray[Math.floor(Math.random() * Math.floor(7))];
+}
+
+
+
 
 
 //add guest
@@ -13,27 +19,15 @@ addGuest.addEventListener("submit", function(e){
     const guestList = document.getElementById("guestList");
     guestList.appendChild(newPara);
 
-    //randomly chooses one guest to be murdered
-    function whoIsDead() {
-    return guestArray[Math.floor(Math.random() * Math.floor(7))];
-    }
-  
-    //assigns mudered guest to a static variable
-    var murderedGuest = whoIsDead();
-  
-    //filters out dead guest so they aren't in play
-    var newGuestList = guestArray.filter(e => e !== murderedGuest);
-  
-    //randomly chooses murderer from new guest list
-    function whoIsMurderer() {
-    return newGuestList[Math.floor(Math.random() * Math.floor(6))];
-    }
-  
-    //assigns murderer to a variable
-    var murderer = whoIsMurderer();
     
     if (guestArray[6] != null){
         guestArray.slice(0, 7);
+        /*const firstRoom = document.forms['irs'];
+        firstRoom.addEventListener("submit", function(e){
+            e.preventDefault();
+            const roomValue = addGuest.querySelector('input[type="text"]').value;
+            newPara.textContent = roomValue;
+        });*/
     } else if (guestArray[5] != null){
         guestArray.push(value);
         newPara.textContent = "The final guest list is: " + guestArray;
@@ -45,31 +39,29 @@ addGuest.addEventListener("submit", function(e){
         var openingStatement = document.getElementById("opening");
         var anotherParagraph = document.createElement("p");
         openingStatement.appendChild(anotherParagraph);
-        anotherParagraph.textContent = "Hello thanks for continuing";
+        var murderedGuest = whoIsDead();
+        anotherParagraph.textContent = "You stand at the estate of " + murderedGuest + ". You have your choice of rooms to visit. Where shall you head first?";
         
         //irs stans for initial room selection
         var irs = document.createElement("form");
-        irs.setAttribute('method',"post");
-        irs.setAttribute('action',"submit.php");
+        //irs.setAttribute('method',"post");
+        //irs.setAttribute('action',"submit.php");
+        irs.setAttribute('id', "irs");
 
         var i = document.createElement("input"); //input element, text
         i.setAttribute('type',"text");
-        i.setAttribute('name',"username");
+        //i.setAttribute('name',"username");
 
         var s = document.createElement("input"); //input element, Submit button
-        s.setAttribute('type',"submit");
-        s.setAttribute('value',"Submit");
+        s.setAttribute('type', "submit");
+        s.setAttribute('id', "nextRoom");
+        s.setAttribute('class', "btn");
+        s.setAttribute('value', "Continue");
 
         irs.appendChild(i);
-        irs.appendChild(s);
+       //irs.appendChild(s);
         anotherParagraph.appendChild(irs);
-
-
-
-
-
-
-
+        anotherParagraph.appendChild(s);
 
         });
 
@@ -79,7 +71,49 @@ addGuest.addEventListener("submit", function(e){
         newPara.textContent = "The current guest list includes: " + guestArray;
     }
 
+
+
 });
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*//randomly chooses one guest to be murdered
+function whoIsDead() {
+    return guestArray[Math.floor(Math.random() * Math.floor(7))];
+}
+
+//assigns mudered guest to a static variable
+const murderedGuest = whoIsDead();
+
+//filters out dead guest so they aren't in play
+var newGuestList = guestArray.filter(e => e !== murderedGuest);
+
+//randomly chooses murderer from new guest list
+function whoIsMurderer() {
+    return newGuestList[Math.floor(Math.random() * Math.floor(6))];
+}
+
+//assigns murderer to a variable
+var murderer = whoIsMurderer();*/
 
