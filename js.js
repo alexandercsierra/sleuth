90a90f921c442c1614b0b86dde87982c45004a71
Alex <alexandercsierra@gmail.com>
2019-09-01 10:30:51 -0400

    form clears after clicking, additional styling of inputs and buttons, no autocomplete

--- js.js
+++ js.js
@@ -22,46 +22,41 @@ addGuest.addEventListener("submit", function(e){
     
     if (guestArray[6] != null){
         guestArray.slice(0, 7);
-        /*const firstRoom = document.forms['irs'];
-        firstRoom.addEventListener("submit", function(e){
-            e.preventDefault();
-            const roomValue = addGuest.querySelector('input[type="text"]').value;
-            newPara.textContent = roomValue;
-        });*/
+
     } else if (guestArray[5] != null){
         guestArray.push(value);
         newPara.textContent = "The final guest list is: " + guestArray;
 
-
         //done adding guests button to start game
         var doneGuest = document.querySelector("#doneGuest");
         doneGuest.addEventListener("click", function(e){
-        var openingStatement = document.getElementById("opening");
-        var anotherParagraph = document.createElement("p");
-        openingStatement.appendChild(anotherParagraph);
-        var murderedGuest = whoIsDead();
-        anotherParagraph.textContent = "You stand at the estate of " + murderedGuest + ". You have your choice of rooms to visit. Where shall you head first?";
+            var openingStatement = document.getElementById("opening");
+            var anotherParagraph = document.createElement("p");
+            openingStatement.appendChild(anotherParagraph);
+            var murderedGuest = whoIsDead();
+            anotherParagraph.textContent = "You stand at the estate of " + murderedGuest + ". You have your choice of rooms to visit. Where shall you head first?";
         
-        //irs stans for initial room selection
-        var irs = document.createElement("form");
-        //irs.setAttribute('method',"post");
-        //irs.setAttribute('action',"submit.php");
-        irs.setAttribute('id', "irs");
+            //irs stans for initial room selection
+            var irs = document.createElement("form");
+            //irs.setAttribute('method',"post");
+            //irs.setAttribute('action',"submit.php");
+            irs.setAttribute('id', "irs");
 
-        var i = document.createElement("input"); //input element, text
-        i.setAttribute('type',"text");
-        //i.setAttribute('name',"username");
+            var i = document.createElement("input"); //input element, text
+            i.setAttribute('type',"text");
+            i.setAttribute('onfocus', "this.value=' '");
+            //i.setAttribute('name',"username");
 
-        var s = document.createElement("input"); //input element, Submit button
-        s.setAttribute('type', "submit");
-        s.setAttribute('id', "nextRoom");
-        s.setAttribute('class', "btn");
-        s.setAttribute('value', "Continue");
+            var s = document.createElement("input"); //input element, Submit button
+            s.setAttribute('type', "submit");
+            s.setAttribute('id', "nextRoom");
+            s.setAttribute('class', "btn");
+            s.setAttribute('value', "Continue");
 
-        irs.appendChild(i);
-       //irs.appendChild(s);
-        anotherParagraph.appendChild(irs);
-        anotherParagraph.appendChild(s);
+            irs.appendChild(i);
+            //irs.appendChild(s);
+            anotherParagraph.appendChild(irs);
+            anotherParagraph.appendChild(s);
 
         });
 
